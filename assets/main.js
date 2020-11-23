a70a68ffe7a8945ad3a53681404091611ead03b5
Ale <alessandrochb@gmail.com>
2020-11-23 16:12:09 +0100

    stampati in pagina i 10 album

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,23 @@
 var app = new Vue ({
     el:'#root',
     data: {
-
+        albums: [],
     },
     methods: {
 
     },
     mounted(){
         console.log('hello world');
+
+        axios.get(' https://flynn.boolean.careers/exercises/api/array/music')
+            .then((element) => {
+                console.log(element);
+                console.log(element.data);
+                console.log(element.data.response);
+                this.albums = element.data.response;
+                console.log(this.albums);
+            })
+
+
     }
 })
