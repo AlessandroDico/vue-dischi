f45bfad7a8eb6a2179781d843aa797b13505ac39
Ale <alessandrochb@gmail.com>
2020-11-23 19:25:21 +0100

    aggiunta possibilità selezione dei dischi

--- assets/main.js
+++ assets/main.js
@@ -2,22 +2,35 @@ var app = new Vue ({
     el:'#root',
     data: {
         albums: [],
+        genreArray: [],
+        valGenre: 'All',
+        genre: 'All',
+
     },
     methods: {
+        selectGenre(){
+            for (var i = 0; i < this.albums.length; i++) {
 
+                if (!this.genreArray.includes(this.albums[i].genre)) {
+                    this.genreArray.push(this.albums[i].genre);
+                }
+            }
+            console.log(this.genreArray);
+        }
     },
     mounted(){
         console.log('hello world');
 
         axios.get(' https://flynn.boolean.careers/exercises/api/array/music')
             .then((element) => {
-                console.log(element);
-                console.log(element.data);
-                console.log(element.data.response);
+                // console.log(element);
+                // console.log(element.data);
+                // console.log(element.data.response);
                 this.albums = element.data.response;
-                console.log(this.albums);
+                // console.log(this.albums);
+                this.selectGenre();
             })
 
-
+        this.valGenre = this.genre;
     }
 })
